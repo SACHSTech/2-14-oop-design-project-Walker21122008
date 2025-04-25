2f435f747751e26ab6197e3b546c645a43fa85f2
hasini2k8 <hasini2k8@gmail.com>
2025-04-25 10:55:47 -0400

    setted up main.java and planning to load the csv files of each info into an array

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static List<Airport> airports = new ArrayList<>();
+    private static List<Passengers> passengers = new ArrayList<>();
+    private static List<Gate> gates = new ArrayList<>();
+    private static List<Terminal> terminals = new ArrayList<>();
     
+    public static void main(String[] args) {
+    
+
+    }
+
 }
