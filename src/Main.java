82d271dd10e413366844f564595e947633d0f3e2
hasini2k8 <hasini2k8@gmail.com>
2025-04-25 12:04:46 -0400

    Made the supUpTheAirports method and formed the frame structure of the other methods it would run

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +11,29 @@ public class Main {
     private static List<Terminal> terminals = new ArrayList<>();
     
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     
 
+    }
+
+    private static void setUpTheAirports() throws IOException{
+
+        loadAirportsFromCSV("Passengers.csv");
+        loadGatesFromCSV("Gates.csv");
+        loadPassengersFromCSV("Passengers.csv");
+
+    }
+
+    private static void loadAirportsFromCSV(String csvFile) throws IOException {
+
+    }
+
+    private static void loadGatesFromCSV(String csvFile) throws IOException {
+        
+    }
+
+    private static void loadPassengersFromCSV(String csvFile) throws IOException {
+    
     }
 
 }
